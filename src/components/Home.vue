fe720affe87cc1856b734deabff758c0db454cdb
ivan-aim <77643196+ivan-aim@users.noreply.github.com>
2023-12-15 17:06:58 -0500

    Swap out VueAdsRandom with iframe ad

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,25 +1,50 @@
 <script setup>
+  import { onMounted, reactive } from 'vue';
   import Timer from '@/components/Timer.vue';
-  import VueAdsRandom from '@/components/VueAds/Random.vue';
+  // import VueAdsRandom from '@/components/VueAds/Random.vue';
+
+
+  // === Data ===
+  const state = reactive({
+    isAdLoaded: false,
+  });
+
+
+  // === Lifecycle Hooks ===
+  /**
+   * Start interval to count down timer if it is switched on
+   */
+  onMounted(() => {
+    setTimeout(() => {
+      // Resize iframe that displays ad from https://ads.ivan-lim.com
+      // @ts-ignore, iFrameResize is made available by iframeResizer.min.js
+      iFrameResize({ log: false }, 'iframe.ads');
+
+      // Display ad element
+      state.isAdLoaded = true;
+    }, 1000);
+  });
 </script>
 
 <template>
   <!-- === Timer Display and Control === -->
   <div class="content row align-items-center">
     <Timer />
-  </div>
 
-  <!-- === Random Advertisement === -->
-  <div class="row pt-5">
-    <div class="col-12 text-center">
-      <VueAdsRandom />
-    </div>
+    <!-- === Random Advertisement === -->
+    <iframe
+      class="col-12 ads mt-5"
+      frameborder="0"
+      src="https://ads.ivan-lim.com"
+      v-show="state.isAdLoaded"
+    />
   </div>
 </template>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 .content {
-  min-height: calc(60vh);
+  /* Middle pane should be full screen height, removing heights of header, footer, and footer top margin (mt-5 = 3rem) */
+  min-height: calc(100vh - 54px - 40px - 3rem) ;
 }
 </style>
--- src/store/constants.js
+++ src/store/constants.js
@@ -1,4 +1,4 @@
 // === App Specific Info ===
 export const appName = 'Pomodoro Timer';
 
-export const appVersion = '2.5.0';
+export const appVersion = '2.6.0';
